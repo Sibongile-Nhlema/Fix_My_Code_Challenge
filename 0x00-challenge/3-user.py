0c8f2f398d8fa36d005cd950015b524a13b8d7d3
Sibongile <sibongile.nhlema@gmail.com>
2024-01-22 02:31:53 -0800

    completd task 3

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -53,7 +53,7 @@ class User():
         if pwd is None or type(pwd) is not str:
             return False
         if self.__password is None:
-            return False
+            return True
         return hashlib.md5(pwd.encode()).hexdigest().upper() == self.__password
 
 
@@ -84,7 +84,7 @@ if __name__ == '__main__':
     if user_2.password is not None:
         print("User.password should be None if setter to an integer")
 
-    if user_1.is_valid_password(u_pwd):
+    if not user_1.is_valid_password(u_pwd):
         print("is_valid_password should return True if it's the right \
 password")
 
